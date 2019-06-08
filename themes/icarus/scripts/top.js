2da2acc9bf28d14ebb5459e5f7daea2f5c42f5c6
cyea <yuyehack@gmail.com>
2019-06-08 14:44:16 +0800

    feat: top

---
+++ themes/icarus/scripts/top.js
@@ -0,0 +1,23 @@
+var pagination = require("hexo-pagination");
+hexo.extend.generator.register("index", function(locals) {
+  var config = this.config;
+  // var posts = locals.posts.sort(config.index_generator.order_by);
+  locals.posts.forEach(function(item) {
+    if (!item.top) {
+      item.top = 0;
+    }
+  });
+  var posts = locals.posts.sort("-top " + config.index_generator.order_by);
+  // var posts = locals.posts.sort({ top: -1 , date: -1 });
+
+  var paginationDir = config.pagination_dir || "page";
+  var path = config.index_generator.path || "";
+  return pagination(path, posts, {
+    perPage: config.index_generator.per_page,
+    layout: ["index", "archive"],
+    format: paginationDir + "/%d/",
+    data: {
+      __index: true
+    }
+  });
+});
