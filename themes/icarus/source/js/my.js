fea4072713ae7b480ee4194d623ad55c9db0b30e
cyea <yuyehack@gmail.com>
2019-06-13 10:48:47 +0800

    feat: add sw

--- themes/icarus/source/js/my.js
+++ themes/icarus/source/js/my.js
@@ -42,4 +42,23 @@
     }
   };
   document.addEventListener(visibilityChangeEvent, onVisibilityChange);
+
+  // Service Worker
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/sw.js", { scope: "/" })
+        .then(function(registration) {
+          // 注册成功
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope
+          );
+        })
+        .catch(function(err) {
+          // 注册失败:(
+          console.log("ServiceWorker registration failed: ", err);
+        });
+    });
+  }
 })(window, document);
